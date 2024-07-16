0ffb935546910be388877eafc171ce0c2bb9ccfb
syedDayim <dayim1277@gmail.com>
2024-07-16 11:55:11 +0530

    Routing

--- Routing In Express/app.js
+++ Routing In Express/app.js
@@ -3,9 +3,23 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || "3000";
 
+const PNF = '<h1 style={color: "red";}>Page not found</h1>'
+
+// Routs
 app.get('/', (req, res)=>{
     res.send('Hello World!');
+});
+
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+
+
+app.all("*", (req, res) => {
+    res.send(PNF)
 })
+
+
 
 
 
