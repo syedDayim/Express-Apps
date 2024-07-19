50d6193d1bc2c0c666e80bf4c69c4012017d57e9
syedDayim <dayim1277@gmail.com>
2024-07-19 11:58:23 +0530

    some updated

--- H_Static Files in Express/app.js
+++ H_Static Files in Express/app.js
@@ -4,8 +4,8 @@ import { join } from "path"
 const app = express();
 const port = process.env.PORT || '3000';
 
-app.use(express.static(join((process.cwd(), 'public'))))
-app.use('/', router)
+app.use(express.static(join((process.cwd(), 'public'))));
+app.use('/', router);
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
